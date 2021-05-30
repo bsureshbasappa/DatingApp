e99a79abf4947778f64f4e48c06739e96d6349f0
sureshbasappa <reach2sureshb@gmail.com>
2021-05-30 11:19:55 +0300

    Final Copy

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -45,7 +45,6 @@ namespace API.Services
                 Subject = new ClaimsIdentity(claims),
                 Expires = DateTime.Now.AddDays(7),
                 SigningCredentials = creds
-
             };
 
             var tokenHandler = new JwtSecurityTokenHandler();
