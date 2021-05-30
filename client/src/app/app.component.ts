0f9afcfeb87b521ba5e41abba0c830978970b52d
sureshbasappa <reach2sureshb@gmail.com>
2021-05-30 11:22:50 +0300

    Final Copy 2

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -9,6 +9,7 @@ import { PresenceService } from './_services/presence.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent implements OnInit{
   title = 'DatingApp';
   users:any;
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -32,6 +32,7 @@ export class NavComponent implements OnInit {
     })
   }
   
+  
   logout(){
     this.accountService.logout();
     this.router.navigateByUrl('/');
